2cb62b0f0ac80b25d6619f2fc996e154f8cb9947
ShehabMohamed21 <53667053+ShehabMohamed21@users.noreply.github.com>
2020-04-14 22:58:42 +0200

    Add files via upload

--- src/Classes/DataStructures/PQueue.java
+++ src/Classes/DataStructures/PQueue.java
@@ -1,7 +1,6 @@
-package Classes.DataStructures;
+package Classes;
 
-import Interfaces.DataStructures.IPriorityQueue;
-
+import Interface.IPriorityQueue;
 
 public class PQueue implements IPriorityQueue {
 
@@ -50,66 +49,72 @@ public class PQueue implements IPriorityQueue {
 
 	@Override
 	public void insert(Object item, int key) {
-		// creating a new node 
+		// creating a new node
 		Node newElement = new Node();
-		//checking if the queue is empty or not
+		// checking if the queue is empty or not
 		if (isEmpty()) {
 			newElement.setValue(item);
 			newElement.setKey(key);
 			front = rear = newElement;
 			size++;
 		} else {
-			// if the queue is not empty assign item values to newElement node 
+			// if the queue is not empty assign item values to newElement node
+			// pointer points on front of the queue
+			// if the key of newElement is smaller than that of pointer
+			// make the newElement as first node and let it be front
 			newElement.setValue(item);
 			newElement.setKey(key);
-			
-			// pointer points on front of the queue
 			Node pointer = front;
 			Node prev;
-			
-			// if the key of newElement is smaller than that of pointer
-			// make the newElement as first node and let it be front 
+
 			if ((int) newElement.getKey() < (int) pointer.getKey()) {
 				newElement.setNext(pointer);
 				front = newElement;
 				size++;
 			} else {
-					// if not so i will iterate through the remaining of the queue to see the correct position
-					prev = pointer;
-					pointer = pointer.getNext();
-					// prev is pointing on the front and pointer point to the next node and their motion will remain the same 
-					// pointer leads prev
-					while (pointer != null) {
-						if ((int) newElement.getKey() < (int) pointer.getKey()) {
-							break;
-						}
-						prev = prev.getNext();
-						pointer = pointer.getNext();
+
+				// if not so i will iterate through the remaining of the queue to see the
+				// correct position
+				// prev is pointing on the front and pointer point to the next node and their
+				// motion will remain the same
+				// pointer leads prev
+
+				prev = pointer;
+				pointer = pointer.getNext();
+				while (pointer != null) {
+					if ((int) newElement.getKey() < (int) pointer.getKey()) {
+						break;
 					}
-					prev.setNext(newElement);
-					newElement.setNext(pointer);
-					size++;
+					prev = prev.getNext();
+					pointer = pointer.getNext();
+				}
+				prev.setNext(newElement);
+				newElement.setNext(pointer);
+				size++;
 			}
-		
+
 		}
-		// looping through the queue to make the rear pointer points to the correct node
-		Node p = front;
-		while (p.getNext()!= null) {
-			p = p.getNext();
+		// if the newElement inserted after rear , it will be rear
+		if (rear.getNext() == newElement) {
+			rear = newElement;
 		}
-		rear = p;
 	}
 
 	@Override
 	public Object removeMin() {
-		// creating a pointer that points to the node wanted to be deleted in order not to lose it 
-		Node pointer = front;
-		
+
+		if (isEmpty()) {
+			throw new IllegalStateException();
+		}
+
+		// creating a pointer that points to the node wanted to be deleted in order not
+		// to lose it
 		// front will move to the next node
+		// setting the next of the pointer equal null in order to be deleted
+		
+		Node pointer = front;
 		front = front.getNext();
 		Object min = pointer.getValue();
-		
-		//setting the next of the pointer equal null in order to be deleted 
 		pointer.setNext(null);
 		size--;
 		return min;
