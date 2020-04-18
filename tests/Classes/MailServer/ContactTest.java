65a14e7ca3bd13ed6886f5d398a9e6f7a1783b11
Ahmad Waleed <a.waleedothman@gmail.com>
2020-04-18 21:16:07 +0200

    Add files via upload

---
+++ tests/Classes/MailServer/ContactTest.java
@@ -0,0 +1,18 @@
+package Classes.MailServer;
+
+import org.junit.jupiter.api.Test;
+
+class ContactTest {
+
+    @Test
+    void addAddress() {
+    }
+
+    @Test
+    void changeAddress() {
+    }
+
+    @Test
+    void getAddressesString() {
+    }
+}
