e79ac76d01bf76e8a25d6d3db1e84281960d5f79
Ahmad Waleed <a.waleedothman@gmail.com>
2020-04-18 21:29:50 +0200

    updates

--- Tests/Classes/MailServer/ContactTest.java
+++ Tests/Classes/MailServer/ContactTest.java
@@ -2,8 +2,6 @@ package Classes.MailServer;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class ContactTest {
 
     @Test
--- Tests/Classes/MailServer/UserTest.java
+++ Tests/Classes/MailServer/UserTest.java
@@ -2,7 +2,6 @@ package Classes.MailServer;
 
 import org.junit.jupiter.api.Test;
 
-import java.io.FileNotFoundException;
 import java.io.IOException;
 
 import static org.junit.jupiter.api.Assertions.*;
